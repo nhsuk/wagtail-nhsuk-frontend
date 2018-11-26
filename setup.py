3423744220af68336e9de5771625adce7f8481ea
Mike Monteith <mike@mikemonteith.com>
2018-11-26 13:34:14 +0000

    Add README.md content to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 INSTALL_REQUIRES = [
     'Wagtail>=2.0',
 ]
@@ -14,6 +17,8 @@ setup(
     description="NHS Frontend Styles for Wagtail",
     author="Mike Monteith",
     author_email="<mike.monteith@nhs.net>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
