979b6821a5c83bffaa11ad6598848dc60f6976eb
Mike Monteith <mike@mikemonteith.com>
2019-01-17 14:16:00 +0000

    Upgrade to latest frontend library

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class CompileCSSCommand(build):
     def run(self):
         filepath_base = 'wagtailnhsstyle/static/wagtailnhsstyle/css/'
         filenames = [
-            'nhsuk-0.1.6.min.css',
+            'nhsuk-0.8.0.min.css',
             'fixes.css',
         ]
 
