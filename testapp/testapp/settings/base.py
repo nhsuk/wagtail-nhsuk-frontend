a4ff16ab3b3aa52dab6d614428b45f7d7ec100b3
Mike Monteith <mike@mikemonteith.com>
2019-01-18 16:34:55 +0000

    Add header template

--- testapp/testapp/settings/base.py
+++ testapp/testapp/settings/base.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'testapp.context_processors.menu_items',
             ],
         },
     },
