72f997ba1fc73fbfdac9a090ab5ebc76310150dc
Mike Monteith <mike@mikemonteith.com>
2019-01-18 17:48:48 +0000

    Use same variable names in testapp and docs

--- testapp/testapp/context_processors.py
+++ testapp/testapp/context_processors.py
@@ -1,18 +1,11 @@
 
 def menu_items(request):
     """Adds links to the context for rendering in the global header navigation"""
-    context = {}
-    context['primary_links'] = [{
-        "label": "One",
-        "url": "/one",
-    }, {
-        "label": "Two",
-        "url": "/two",
-    }, {
-        "label": "Three",
-        "url": "/three",
-    }, {
-        "label": "Four",
-        "url": "/four",
-    }]
-    return context
+    return {
+        'navigation': [
+          { "label": "One", "url": "/one" },
+          { "label": "Two", "url": "/two" },
+          { "label": "Three", "url": "/three" },
+          { "label": "Four", "url": "/four" },
+        ]
+    }
