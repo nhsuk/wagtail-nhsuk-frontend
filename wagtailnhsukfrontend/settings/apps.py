d265feed52e53245ddc5bd1b7a1d670273417921
Mike Monteith <mike@mikemonteith.com>
2019-02-18 16:27:14 +0000

    Add linting
    Linting with flake8.
    Failures fixed or supressed

--- wagtailnhsukfrontend/settings/apps.py
+++ wagtailnhsukfrontend/settings/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
+
 class SettingsAppConfig(AppConfig):
     name = 'wagtailnhsukfrontend.settings'
     label = 'wagtailnhsukfrontendsettings'
--- wagtailnhsukfrontend/settings/templatetags/nhsukfrontendsettings_tags.py
+++ wagtailnhsukfrontend/settings/templatetags/nhsukfrontendsettings_tags.py
@@ -3,6 +3,7 @@ from wagtailnhsukfrontend.settings.models import HeaderSettings
 
 register = template.Library()
 
+
 @register.inclusion_tag('wagtailnhsukfrontend/header.html', takes_context=True)
 def header(context):
     page = context['page']
@@ -17,7 +18,7 @@ def header(context):
         'logo_href': header.logo_link.relative_url(site) if header.logo_link else '',
         'logo_aria': header.logo_aria,
         'show_search': header.show_search,
-        'primary_links' : [
+        'primary_links': [
             {
                 'label': link.label,
                 'url': link.page.relative_url(site)
