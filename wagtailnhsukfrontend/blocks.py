f5b56081e1807e21f23f011108e50dd1e278d862
Sab <sabeeh.ul-hassan1@nhs.net>
2019-02-19 11:58:39 +0000

    Fix variable names for review date component

--- wagtailnhsukfrontend/blocks.py
+++ wagtailnhsukfrontend/blocks.py
@@ -55,11 +55,11 @@ class BackLinkBlock(StaticBlock):
 
 class ReviewDateBlock(StructBlock):
 
-    revieweddate = DateBlock(label="Enter the reviewed date", required=True,)
-    nextreviewdate = DateBlock(label="Enter the next review date", required=False)
+    reviewed_date = DateBlock(label="Enter the reviewed date", required=True,)
+    next_review_date = DateBlock(label="Enter the next review date", required=False)
 
-    lastReviewLabel = CharBlock(default="Last review date ", label="Last review date", required=False)
-    nextReviewLabel = CharBlock(default="Next review due ", label="Next review due ", required=False)
+    last_review_label = CharBlock(default="Last review date ", label="Last review date", required=False)
+    next_review_label = CharBlock(default="Next review due ", label="Next review due ", required=False)
 
     class Meta:
         template = 'wagtailnhsukfrontend/review_date.html'
