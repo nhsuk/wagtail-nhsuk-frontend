e8cf482b8a583bf6384e797da4e75a8f680e4dd6
Mike Monteith <mike@mikemonteith.com>
2019-03-01 15:45:22 +0000

    Add forms models to the testapp

---
+++ testapp/forms/models.py
@@ -0,0 +1,13 @@
+from wagtail.core.models import Page
+
+from .forms import BigForm
+
+
+class FormPage(Page):
+
+    parent_page_types = ['home.HomePage']
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['form'] = BigForm(request.POST or None)
+        return context
