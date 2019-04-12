6a8602d7d89dbc718340e93927d19ee7ef158527
Mike Monteith <mike@mikemonteith.com>
2019-04-12 14:51:07 +0100

    Release version 0.2.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'build': CompileCSSCommand,
     },
     name="wagtail-nhsuk-frontend",
-    version="0.1.0",
+    version="0.2.0",
     description="NHSUK Frontend Styles for Wagtail",
     author="Mike Monteith",
     author_email="<mike.monteith@nhs.net>",
