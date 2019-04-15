9e974b6c3369fd6481d35ae9a1e8c69cb1555510
Sab <sabeeh.ul-hassan1@nhs.net>
2019-04-15 13:29:07 +0100

    Add promo component and hubs page
    Apply fixes
    
    Add promo block
    
    Add contents list component
    
    - Template tag to generate siblings list
    - Template include can also be used
    
    Migrations fix
    
    Delete 0005_hubs_page.py
    
    Delete 0006_hubs_page.py
    
    Fix migrations 2
    
    Fixes
    
    Delete 0006_hubspage.py

---
+++ docs/components/contents_list.md
@@ -0,0 +1,40 @@
+# Contents List
+
+## Templatetag
+
+```django
+{% load nhsukfrontend_tags %}
+
+...
+
+{% contents_list %}
+```
+
+The `{% contents_list %}` tag will render a list of sibling pages.
+
+`page.get_url(request)` is used to generate the page url.
+
+`page.title` is used for the labels.
+
+## Template include
+
+```django
+{% include 'wagtailnhsukfrontend/contents_list.html' with links=your_links_array %}
+```
+
+Including the `wagtailnhsukfrontend/contents_list.html` template will render the contents list.
+
+An array of links should be passed as a template variable
+
+| Option | Description | Default |
+| ------ | ----------- | ------- |
+| `links` | Array of dicts | [] |
+| `links[].label` | Text to display as the link label | |
+| `links[].href` | URL to use as the link href | |
+| `links[].is_current` | This link is the current page. It will be displayed in a bold font | |
+
+
+## Reference
+
+* [Service Manual](https://beta.nhs.uk/service-manual/styles-components-patterns/contents-list)
+* [Frontend Library](https://github.com/nhsuk/nhsuk-frontend/tree/master/packages/components/contents-list)
---
+++ tests/test_contents_list_tag.py
@@ -0,0 +1,49 @@
+import pytest
+from django.test import RequestFactory
+from wagtail.core.models import Page
+
+from wagtailnhsukfrontend.templatetags.nhsukfrontend_tags import contents_list
+
+
+def get_contents_list_context(page=None):
+    """Get the contents_list context that will be passed to the template."""
+    page = page or Page.objects.get(url_path='/home/pagination/pagination-page-1/')
+    fake_context = {
+        'page': page,
+        'request': RequestFactory().get('/fake/url/')
+    }
+    # The contents_list tag is an inclusion_tag which returns a new context.
+    new_context = contents_list(fake_context)
+    return new_context
+
+
+@pytest.mark.django_db
+def test_contents_list_siblings_are_dicts(db, django_db_setup):
+    template_context = get_contents_list_context()
+    for item in template_context['links']:
+        assert isinstance(item, dict)
+
+
+@pytest.mark.django_db
+def test_contents_list_length(db, django_db_setup):
+    template_context = get_contents_list_context()
+    # length = 4 ensures that our unpublished page didn't appear
+    assert len(template_context['links']) == 4
+
+
+@pytest.mark.django_db
+def test_contents_list_is_current_page(db, django_db_setup):
+    page = Page.objects.get(url_path='/home/pagination/pagination-page-3/')
+    template_context = get_contents_list_context(page=page)
+    assert template_context['links'][2]['is_current'] is True
+    assert template_context['links'][3]['is_current'] is False
+
+
+def test_contents_list_has_href(db, django_db_setup):
+    template_context = get_contents_list_context()
+    assert template_context['links'][0]['href'] == '/pagination/pagination-page-1/'
+
+
+def test_contents_list_has_label(db, django_db_setup):
+    template_context = get_contents_list_context()
+    assert template_context['links'][0]['label'] == 'Pagination page 1'
