67058c506c76079e8636c65ec43c4e5423817e98
xLasercut <tianxun.wang2004@gmail.com>
2019-04-18 16:38:17 +0100

    fixed scss issues

--- setup.py
+++ setup.py
@@ -13,28 +13,7 @@ TESTING_REQUIRES = [
 ]
 
 
-class CompileCSSCommand(build):
-    """Combine CSS from the frontend library with our wagtail-specific fixes"""
-
-    def run(self):
-        filepath_base = 'wagtailnhsukfrontend/static/wagtailnhsukfrontend/css/'
-        filenames = [
-            'nhsuk-2.1.0.min.css',
-            'fixes.css',
-            'main.css'
-        ]
-
-        with open(filepath_base + 'wagtail-nhsuk-frontend.min.css', 'w') as outfile:
-            for fname in filenames:
-                with open(filepath_base + fname) as infile:
-                    for line in infile:
-                        outfile.write(line)
-
-
 setup(
-    cmdclass={
-        'build': CompileCSSCommand,
-    },
     name="wagtail-nhsuk-frontend",
     version="0.2.0",
     description="NHSUK Frontend Styles for Wagtail",
@@ -46,5 +25,13 @@ setup(
     install_requires=INSTALL_REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    extras_require={'testing': TESTING_REQUIRES}
+    extras_require={'testing': TESTING_REQUIRES},
+    setup_requires=['libsass >= 0.6.0'],
+    sass_manifests={
+        'wagtailnhsukfrontend': {
+            'sass_path': 'static/wagtailnhsukfrontend/sass',
+            'css_path': 'static/wagtailnhsukfrontend/css',
+            'strip_extension': True,
+        }
+    }
 )
