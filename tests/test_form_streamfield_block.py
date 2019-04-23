1c20b6cbfc3db69bfcd7e3a0e6ac05ec161b3c1c
xLasercut <tianxun.wang2004@gmail.com>
2019-04-23 16:44:54 +0100

    added selenuim tests for all form blocks

--- tests/test_form_streamfield_block.py
+++ tests/test_form_streamfield_block.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import Select
 
 class FormStreamfieldBlockTests(unittest.TestCase):
     
@@ -42,16 +43,135 @@ class FormStreamfieldBlockTests(unittest.TestCase):
             error_msg = 'Form item with id: id_{} has incorrect label mapping'.format(field)
             self.assertFalse(element == self.driver.switch_to.active_element, error_msg)
             
+    def test_hint_text(self):
+        hint_map = {
+            'input-normal': 'input hint',
+            'select': 'select hint',
+            'textarea': 'textarea hint',
+            'checkbox': 'checkbox hint',
+            'radio': 'radio hint'
+        }
         
-    def test_input_field_text(self):       
+        for field in hint_map:
+            hint_text = hint_map.get(field)
+            self._assert_hint_text(field, hint_text)
+            
+        
+    def test_input_missing_field_error_msg(self): 
         fields = ['input-normal', 'input-width-2', 'input-disabled',
                   'input-width-3', 'input-width-4', 'input-width-5',
                   'input-width-10', 'input-width-20']
         
-        for field in fields:
-            input_id = 'id_{}'.format(field)
-            
+        self._submit_form()
         
+        for field in fields:
+            if field == 'input-normal':
+                error_text = 'custom missing field error input'
+            else:
+                error_text = 'This field is required'
+            self._assert_error_text(field, error_text)
+    
+    def test_input_validation_error_msg(self):
+        input = self._get_form_item('input-normal')
+        input.send_keys('hello world')
+        self._submit_form()
+        
+        self._assert_error_text('input-normal', 'custom validation error input')
+        
+    def test_input_validation(self):
+        fields = ['input-normal', 'input-width-2',
+                  'input-width-3', 'input-width-4', 'input-width-5',
+                  'input-width-10', 'input-width-20']
+        
+        for field in fields:
+            input = self._get_form_item(field)
+            input.send_keys('test')
+            self._submit_form()
+            self._assert_no_error_text(field)
+        
+    def test_select_missing_field_error_msg(self):
+        self._submit_form()
+        self._assert_error_text('select-disabled', 'This field is required')
+        
+    def test_select_validation_error_msg(self):
+        self._submit_form()
+        self._assert_error_text('select', 'custom validation error select')
+        
+    def test_select_validation(self):
+        select = Select(self._get_form_item('select'))
+        select.select_by_visible_text('select 2')
+        self._submit_form()
+        self._assert_no_error_text('select')
+        
+    def test_textarea_missing_field_error_msg(self):
+        fields = ['textarea', 'textarea-disabled']
+        
+        self._submit_form()
+        
+        for field in fields:
+            if field == 'textarea':
+                error_text = 'custom missing field error ta'
+            else:
+                error_text = 'This field is required'
+            
+            self._assert_error_text(field, error_text)
+    
+    def test_textarea_validation_error_msg(self):
+        textarea = self._get_form_item('textarea')
+        textarea.send_keys('test 123')
+        self._submit_form()
+        self._assert_error_text('textarea', 'custom validation error ta')
+        
+    def test_textarea_validation(self):
+        textarea = self._get_form_item('textarea')
+        textarea.send_keys('test')
+        self._submit_form()
+        self._assert_no_error_text('textarea')
+        
+    def test_checkbox_missing_field_error_msg(self):
+        fields = ['checkbox', 'checkbox-disabled']
+        
+        self._submit_form()
+       
+        for field in fields:
+            if field == 'checkbox':
+                error_text = 'custom missing field error checkbox'
+            else:
+                error_text = 'This field is required'
+            self._assert_error_text(field, error_text)
+            
+    def test_checkbox_validation_error_msg(self):
+        self._click_checkbox_radio_label('Paris')
+        self._submit_form()
+        self._assert_error_text('checkbox', 'custom validation error checkbox')
+        
+    def test_checkbox_validation(self):
+        self._click_checkbox_radio_label('London')
+        self._submit_form()
+        self._assert_no_error_text('checkbox')
+        
+    def test_radio_missing_field_error_msg(self):
+        fields = ['radio', 'radio-disabled']
+        
+        self._submit_form()
+        
+        for field in fields:
+            if field == 'radio':
+                error_text = 'custom missing field error radio'
+            else:
+                error_text = 'This field is required'
+            
+            self._assert_error_text(field, error_text)
+            
+    def test_radio_validation_error_msg(self):
+        self._click_checkbox_radio_label('No')
+        self._submit_form()
+        self._assert_error_text('radio', 'custom validation error radio')
+        
+    def test_radio_validation(self):
+        self._click_checkbox_radio_label('Yes')
+        self._submit_form()
+        self._assert_no_error_text('radio')
     
     def _submit_form(self):
         submit_button = self.driver.find_element_by_id('form-submit-button')
@@ -68,3 +188,17 @@ class FormStreamfieldBlockTests(unittest.TestCase):
     def _click_label(self, id):
         label = self.driver.find_element_by_id('{}-label'.format(id))
         label.click()
+        
+    def _assert_no_error_text(self, id):
+        errors = self.driver.find_elements_by_id('{}-error'.format(id))
+        self.assertFalse(errors)
+        
+    def _get_form_item(self, id):
+        return self.driver.find_element_by_id('id_{}'.format(id))
+    
+    def _click_checkbox_radio_label(self, label_text):
+        labels = self.driver.find_elements_by_tag_name('label')
+        for label in labels:
+            if label.text == label_text:
+                label.click()
+                break
