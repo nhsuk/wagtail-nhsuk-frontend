d6bcfa1ab1d1fd2e46f84c6382e8252747b5bd4e
AdamChrimes <iChrimes@live.co.uk>
2020-04-14 22:40:15 +0100

    [refs #143] rename the label for the Don't list in the admin panel

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 
 - Use the latest version of the NHS.UK frontend library ([v3.0.4](https://github.com/nhsuk/nhsuk-frontend/blob/master/CHANGELOG.md#304---24-march-2020))
 - Add logo and open graph assets
+- Rename the label for the Don't list in the admin panel
 
 ## v0.4.1
 
--- wagtailnhsukfrontend/blocks.py
+++ wagtailnhsukfrontend/blocks.py
@@ -101,7 +101,7 @@ class DontBlock(FlattenValueContext, StructBlock):
 
     heading_level = IntegerBlock(required=True, min_value=2, max_value=4, default=3, help_text='The heading level affects users with screen readers. Default=3, Min=2, Max=4.')
 
-    dont = ListBlock(RichTextBlock)
+    dont = ListBlock(RichTextBlock, label="Don't")
 
     class Meta:
         icon = 'cross'
