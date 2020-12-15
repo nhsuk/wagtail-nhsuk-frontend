04503ceb97f7295531bf0161fb1845efeb167a75
Nick Moreton <nickmoreton@me.com>
2020-12-15 22:20:17 +0000

    corrected test action block external url span tag selector

--- tests/test_action_block.py
+++ tests/test_action_block.py
@@ -8,7 +8,7 @@ def test_action_block_external_url(db, django_db_setup, client: Client):
     response = client.get('/')
     soup = BeautifulSoup(response.content, 'html.parser')
     a_tag = soup.select_one('div.external-url a.nhsuk-action-link__link')
-    span_tag = soup.select_one('span.nhsuk-action-link__text')
+    span_tag = a_tag.select_one('span.nhsuk-action-link__text')
 
     assert a_tag['href'] == 'https://example.com'
     assert span_tag.text == 'Action Link (external url)'
