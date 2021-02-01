6cb384fd1212c0aaec47e4632bb7631d6113071e
Nick Moreton <nickmoreton@me.com>
2021-02-01 05:07:41 +0000

    added test for footer links

--- tests/test_footer_links.py
+++ tests/test_footer_links.py
@@ -7,6 +7,16 @@ import pytest
 def test_footer_links(db, django_db_setup, client: Client):
     response = client.get('/')
     soup = BeautifulSoup(response.content, 'html.parser')
-    # a_tag = soup.select_one('.block-card_clickable:nth-of-type(2) a')
+    footer_links = soup.select('.nhsuk-footer__list-item')
 
-    # assert a_tag['href'] == 'https://example.com/'
+    a_tag_external_link = footer_links[0].select_one('a')
+    assert a_tag_external_link['href'] == 'http://www.nhs.uk'
+    assert a_tag_external_link.text == 'NHS Website'
+
+    a_tag_internal_link_using_alternative_title = footer_links[1].select_one('a')
+    assert a_tag_internal_link_using_alternative_title['href'] == '/promo-hub/'
+    assert a_tag_internal_link_using_alternative_title.text == 'Card Examples'
+
+    a_tag_internal_link_using_page_title = footer_links[2].select_one('a')
+    assert a_tag_internal_link_using_page_title['href'] == '/'
+    assert a_tag_internal_link_using_page_title.text == 'Home'
